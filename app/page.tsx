005c2aee7ed7c29363017dde293c0f6f569df9b5
kamiltomczuk <s24802@pjwstk.edu.pl>
2023-05-22 21:16:15 +0200

    Added Header, Sidebar and route.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
-
+import { DocumentMagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 function Homepage() {
   return (
-    <div>
-        <h1>Hello World</h1>
+    <div className="flex flex-col items-center justify-center">
+      <DocumentMagnifyingGlassIcon className="h-64 w-64 text-indigo-600/20" />
+
+      <h1 className="text-3xl text-center mt-2 text-black font-bold mb-5">
+        Welcome to the web Scraper
+      </h1>
+
+      <h2 className="text-lg italic text-center text-black/50">
+        To see my other projects please visit my github page
+      </h2>
+      <h3 className="text-lg italic text-center text-black/50">
+        <a href="https://github.com/kamiltomczuk">github.com/kamiltomczuk</a>
+      </h3>
     </div>
-  )
+  );
 }
 
 export default Homepage;
---
+++ components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { DocumentMagnifyingGlassIcon } from "@heroicons/react/24/solid";
+
+function Sidebar() {
+  return (
+    <div className="p-2 md:p-10 py-6 overflow-y-auto border-b border-indigo-500/50">
+      <div className="flex flex-col items-center justify-center mb-10">
+        <DocumentMagnifyingGlassIcon className="h-616 w-16 text-indigo-600" />
+
+        <h1 className="hidden md:inline text-center text-3xl mt-2 mb-2 font-bold">
+          Web Scraper
+        </h1>
+        <h2 className="hidden md:inline text-center text-xs italic">
+          Scraping the unscrapable
+        </h2>
+      </div>
+
+      <ul>
+        {/* Sidebar row */}
+        {/* Sidebar row */}
+        {/* Sidebar row */}
+        {/* Sidebar row */}
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
